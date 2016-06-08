0d09ecb9674cd1a54bacdceea20a187eefcfb49a
jchiou <jessica.chiou@gmail.com>
2016-06-08 16:23:05 -0700

    Change var storage to array, pass all tests for queue methods

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,9 +1,10 @@
-var Queue = function() {
+/*var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
   var counter = 0;
+  var first;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
@@ -12,10 +13,43 @@ var Queue = function() {
   };
 
   someInstance.dequeue = function() {
+    if (counter !== 0) {
+      //var keys = Object.keys(storage);
+      //first = counter - keys.length;
+      first = storage[0];
+      delete storage[0];
+      console.log(storage);
+      counter--;
+      return first;
+    }
   };
 
   someInstance.size = function() {
     return counter;
+  };
+
+  return someInstance;
+};*/
+var Queue = function() {
+  var someInstance = {};
+
+  // Use an object with numeric keys to store values
+  var storage = [];
+  // Implement the methods below
+
+  someInstance.enqueue = function(value) {
+    storage.push(value);
+  };
+
+  someInstance.dequeue = function() {
+    if (storage.length !== 0) {
+      //storage = storage.slice(1);
+      return storage.shift();
+    }
+  };
+
+  someInstance.size = function() {
+    return storage.length;
   };
 
   return someInstance;
