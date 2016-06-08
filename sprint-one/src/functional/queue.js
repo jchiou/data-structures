4c1f1dff583f22153206086b7c6df3bc4c7c2aea
jchiou <jessica.chiou@gmail.com>
2016-06-08 15:02:33 -0700

    Return size of zero after new quere, return size of 2 after adding two items, does not error when removing from empty queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,19 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var counter = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.dequeue = function() {
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
