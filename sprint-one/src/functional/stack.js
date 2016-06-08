8c1c19f320b013ba23ee6b7bdcfbbc40bd2bdcec
Jessica Chiou <jessica.chiou@gmail.com>
2016-06-08 12:07:48 -0700

    Return 0 for empty array

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,19 @@
 var Stack = function() {
   var someInstance = {};
-
+  var key = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[key] = value;
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
+    return key;
   };
 
   return someInstance;
