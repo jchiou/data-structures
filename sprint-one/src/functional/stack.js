5cda6a7c2bfafb00cba4c3db768964a4590e1837
jchiou <jessica.chiou@gmail.com>
2016-06-08 14:53:39 -0700

    Return size of zero for new stack, size of two after adding two items, and does not error when removing from empty stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,19 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
