ebeda94b3e323a30300b6c234ad94aa9956beccb
jchiou <jessica.chiou@gmail.com>
2016-06-08 20:14:35 -0700

    Compete pseudoclassical pattern tests for stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,20 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = [];
 };
 
+Stack.prototype.size = function() {
+  return this.storage.length;
+};
+
+Stack.prototype.push = function(val) {
+  return this.storage.push(val);
+};
+
+Stack.prototype.pop = function() {
+  if (this.storage.length !== 0) {
+    return this.storage.pop();
+  }
+};
 
