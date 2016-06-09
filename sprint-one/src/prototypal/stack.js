73002d909d36dd88f670d1fa596e2aac907831f8
jchiou <jessica.chiou@gmail.com>
2016-06-08 20:07:12 -0700

    Pass prototypal pattern tests for both stack and queue

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // but try not not reference your old code in writing the new style.]
+  var instance = Object.create(stackMethods);
+  instance.storage = [];
+  return instance;
 };
 
 var stackMethods = {};
 
+stackMethods.size = function() {
+  return this.storage.length;
+};
 
+stackMethods.push = function(val) {
+  this.storage.push(val);
+  return this.storage.length;
+};
+
+stackMethods.pop = function() {
+  return this.storage.pop();
+};
