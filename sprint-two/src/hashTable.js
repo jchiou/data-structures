fe125f24ab92c49dc5eff13cf560de2424af56c0
jchiou <jessica.chiou@gmail.com>
2016-06-10 21:01:12 -0700

    Complete all specs for Hash Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,7 +7,8 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  if (!Array.isArray(index)) {
+
+  if (!Array.isArray(this._storage[index])) {
     this._storage[index] = [];
     this._storage[index].push([k, v]);
   } else {
@@ -22,16 +23,15 @@ HashTable.prototype.retrieve = function(k) {
   this._storage[index].forEach(function(pair) {
     if (pair[0] === k) {
       value = pair[1];
-      console.log(pair);
     }
   });
-  console.log(value);
+
   return value;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  //this._storage[index][1] = undefined;
+
   this._storage[index].forEach(function(pair, i) {
     if (pair[0] === k) {
       pair.splice(i, 1);
