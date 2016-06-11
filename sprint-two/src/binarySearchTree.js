16fa27b25e61456e4d619c29338f34825b2ebc92
jchiou <jessica.chiou@gmail.com>
2016-06-11 10:20:06 -0700

    Complete binarySearchTree and pass all specs

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,43 @@
 var BinarySearchTree = function(value) {
+  var tree = Object.create(binaryTreeMethods);
+
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+
+  tree.storage = [];
+  tree.storage.push(value);
+  return tree;
+};
+
+var binaryTreeMethods = {};
+
+binaryTreeMethods.insert = function(value) {
+  this.storage.push(value);
+  // return this.value > value ? this.left = BinarySearchTree(value) : this.right = BinarySearchTree(value);
+  if (this.value > value) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (this.right === null) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+binaryTreeMethods.contains = function(value) {
+  return this.storage.indexOf(value) > -1;
+};
+
+binaryTreeMethods.depthFirstLog = function(fn) {
+  this.storage.forEach(function(item) {
+    fn(item);
+  });
 };
 
 
