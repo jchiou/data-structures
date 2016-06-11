8f24cf949c060e56fe2ca3e85a84e575279a5942
jchiou <jessica.chiou@gmail.com>
2016-06-10 19:39:05 -0700

    Pass first five tests for hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,36 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (!Array.isArray(index)) {
+    this._storage[index] = [];
+    this._storage[index].push([k, v]);
+  } else {
+    this._storage[index].push([k, v]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var value;
+
+  this._storage[index].forEach(function(pair) {
+    if (pair[0] === k) {
+      value = pair[1];
+      console.log(pair);
+    }
+  });
+  console.log(value);
+  return value;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //this._storage[index][1] = undefined;
+  this._storage[index].forEach(function(pair, i) {
+    if (pair[0] === k) {
+      pair.splice(i, 1);
+    }
+  });
 };
 
 
